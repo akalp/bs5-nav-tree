45d383694bcf1c71886c6feb639c5800430e9cdd
Hasan Akalp <hasan.akalp@gmail.com>
2021-10-16 18:22:30 +0300

    add extra option settings and change option setting way

--- example/static/js/tree.js
+++ example/static/js/tree.js
@@ -1,46 +1,59 @@
-function copyAttrs(src, target) {
-  for (let attr of src.attributes) {
-    if (attr.name == "class")
-      attr.value.split(" ").forEach((cls) => target.classList.add(cls))
-    else
-      target.setAttribute(attr.name, attr.value);
+var NavTree = function (options) {
+  var element = document.querySelector(options.selector);
+
+  var props = {
+    element: document.querySelector(options.selector),
+    searchable: options.searchable || false,
+    showEmptyGroups: options.showEmptyGroups || false,
+    groupOpenIconClass: (options.groupOpenIconClass && options.groupOpenIcon) ? options.groupOpenIconClass : "fas",
+    groupOpenIcon: (options.groupOpenIconClass && options.groupOpenIcon) ? options.groupOpenIcon : "fa-chevron-down",
+    groupCloseIconClass: (options.groupCloseIconClass && options.groupCloseIcon) ? options.groupCloseIconClass : "fas",
+    groupCloseIcon: (options.groupCloseIconClass && options.groupCloseIcon) ? options.groupCloseIcon : "fa-chevron-right",
+    linkIconClass: (options.linkIconClass && options.linkIcon) ? options.linkIconClass : "fas",
+    linkIcon: (options.linkIconClass && options.linkIcon) ? options.linkIcon : "fa-link",
+    searchPlaceholderText: options.searchPlaceholderText || ""
   }
-}
+
+  var getIconWithWrapper = (iconClass, icon) => {
+    var wrapper = document.createElement('span');
+    wrapper.classList.add("d-inline-block", "tree-icon");
+    wrapper.style.width = '25px';
+    wrapper.innerHTML = `<i class="${iconClass} ${icon}"></i>`;
+    return wrapper;
+  }
 
 
-var NavTree = function (props) {
-  this.props = props;
-  this.element = document.querySelector(this.props.obj_id);
-  this.searchable = this.element.dataset.hasOwnProperty('searchable');
-  this.showEmptyGroups = this.element.dataset.hasOwnProperty('showEmptyGroups');
-  this.chevronIcon = `<i class="fas fa-chevron-right"></i>`;
-  this.linkIcon = `<i class="fas fa-link"></i>`;
-  this.searchInput = `<input type="text" name="menu-tree-search" id="menu-tree-search" class="form-control" placeholder="Search">`;
+  var copyAttrs = (src, target) => {
+    for (let attr of src.attributes) {
+      if (attr.name == "class")
+        attr.value.split(" ").forEach((cls) => target.classList.add(cls))
+      else
+        target.setAttribute(attr.name, attr.value);
+    }
+  }
 
-  this.init = function () {
-    if (!this.parent) {
-      var parent = document.createElement('div');
-      parent.setAttribute('id', 'nav-tree-wrapper');
-      this.element.parentElement.replaceChild(parent, this.element);
-      parent.appendChild(this.element);
-      this.parent = parent;
+
+  var init = function () {
+    if (!props.parent) {
+      var new_parent = document.createElement('div');
+      new_parent.setAttribute('id', 'nav-tree-wrapper');
+      element.parentElement.replaceChild(new_parent, element);
+      new_parent.appendChild(element);
+      props.parent = new_parent;
     }
 
-    this.element.classList.add("nav", "flex-column");
+    element.classList.add("nav", "flex-column");
 
-    this.element.querySelectorAll("ul").forEach((ul) => ul.classList.add("nav", "flex-column")); // add bs5 classes to lists
+    element.querySelectorAll("ul").forEach((ul) => ul.classList.add("nav", "flex-column")); // add bs5 classes to lists
 
-    this.element.querySelectorAll("li").forEach((li) => {
+    element.querySelectorAll("li").forEach((li) => {
       const li_id = li.getAttribute("id");
       li.classList.add("nav-item"); // add bs5 class to list item
 
       const a = li.querySelector("a");
       a.classList.add("nav-link");
       a.setAttribute("id", "tree-link-" + li_id);
-      var icon_parent = document.createElement('span');
-      icon_parent.classList.add("d-inline-block", "tree-icon");
-      icon_parent.style.width = '25px';
-      a.prepend(icon_parent);
+
 
       if (li.querySelector("ul")) { // check list item has a ul object
         a.classList.add("tree-group-link");
@@ -49,7 +62,7 @@ var NavTree = function (props) {
         a.setAttribute("aria-expanded", "false");
         a.setAttribute("href", "#nav-tree-list-wrapper-" + li_id);
         a.setAttribute("aria-controls", "nav-tree-list-wrapper-" + li_id);
-        icon_parent.innerHTML = this.chevronIcon;
+        a.prepend(getIconWithWrapper(props.groupCloseIconClass, props.groupCloseIcon));
 
 
         const ul = li.querySelector("ul");
@@ -62,11 +75,14 @@ var NavTree = function (props) {
         new bootstrap.Collapse(collapsable, { toggle: false });
 
         collapsable.addEventListener('show.bs.collapse', (e) => {
-          e.target.parentElement.querySelector('.tree-icon i').classList.replace('fa-chevron-right', 'fa-chevron-down');
+          e.target.parentElement.querySelector('.tree-icon i').classList.replace(props.groupCloseIcon, props.groupOpenIcon);
+          e.target.parentElement.querySelector('.tree-icon i').classList.replace(props.groupCloseIconClass, props.groupOpenIconClass);
         });
 
         collapsable.addEventListener('hide.bs.collapse', (e) => {
-          e.target.parentElement.querySelector('.tree-icon i').classList.replace('fa-chevron-down', 'fa-chevron-right');
+          e.target.parentElement.querySelector('.tree-icon i').classList.replace(props.groupOpenIcon, props.groupCloseIcon);
+          e.target.parentElement.querySelector('.tree-icon i').classList.replace(props.groupOpenIconClass, props.groupCloseIconClass);
+
           if (e.target.querySelector('.collapse'))
             bootstrap.Collapse.getInstance(e.target.querySelector('.collapse')).hide();
         });
@@ -75,10 +91,10 @@ var NavTree = function (props) {
 
         a.classList.add("tree-link");
 
-        icon_parent.innerHTML = this.linkIcon;
+        a.prepend(getIconWithWrapper(props.linkIconClass, props.linkIcon));
 
         a.addEventListener('click', () => {
-          this.parent.querySelectorAll('a.tree-link').forEach((link) => {
+          props.parent.querySelectorAll('a.tree-link').forEach((link) => {
             link.classList.remove('active');
           });
 
@@ -86,15 +102,17 @@ var NavTree = function (props) {
         });
 
       }
-      icon_parent.firstElementChild.setAttribute('id', 'tree-icon-' + li_id);
+
+      a.querySelector('.tree-icon i').setAttribute('id', 'tree-icon-' + li_id);
+
     });
 
-    if (this.searchable) {
+    if (props.searchable) {
       var search_parent = document.createElement('div');
       search_parent.setAttribute('id', 'nav-tree-search-wrapper');
-      search_parent.classList.add('my-2');
-      this.parent.prepend(search_parent);
-      search_parent.innerHTML = this.searchInput;
+      search_parent.classList.add('mx-3', 'my-2');
+      props.parent.prepend(search_parent);
+      search_parent.innerHTML = `<input type="text" name="menu-tree-search" id="menu-tree-search" class="form-control" placeholder="${props.searchPlaceholderText}">`;
 
       search_parent.firstChild.addEventListener('keyup', (e) => {
         const nodes = Array.from(document.querySelectorAll("li"));
@@ -109,29 +127,29 @@ var NavTree = function (props) {
               bootstrap.Collapse.getInstance(c).show();
               node.classList.remove('d-none');
               c.classList.remove('no-transition');
-            } else if (!this.showEmptyGroups) {
+            } else if (!props.showEmptyGroups) {
               node.classList.add('d-none');
             }
           });
         }
-
       });
-
     }
   }
 
   this.update = function (menu_html) {
-    const old = this.element.cloneNode(true);
-    this.parent.innerHTML = menu_html;
-    this.element = this.parent.querySelector('ul');
+    const old = element.cloneNode(true);
+    props.parent.innerHTML = menu_html;
+    element = props.parent.querySelector('ul');
     this.init();
     Array.from(old.querySelectorAll("[id]")).map((i) => i.getAttribute("id")).forEach((id) => {
-      var new_node = this.element.querySelector("#" + id);
+      var new_node = element.querySelector("#" + id);
       var old_node = old.querySelector("#" + id);
       if (new_node && old_node)
         copyAttrs(old_node, new_node);
     });
   }
 
-  this.init();
+
+
+  init();
 }
